1e51abc6cb351a582f0ebd59e5de95d3e5d90dab
Rocha Nahuel <rocha.nahuel@hotmail.com>
2024-10-06 15:22:53 -0300

    push

---
+++ src/test/java/com/app/waki/WakiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.waki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WakiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
